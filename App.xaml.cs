66d4a2eab38c227a50674393b7a88ab696bba761
pialvic <pialvic@gmail.com>
2025-09-26 16:25:03 +0300

    логика нажатий

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,8 +7,5 @@ namespace headFirst
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
-    {
-    }
-
+    public partial class App : Application { }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -34,18 +34,41 @@ namespace headFirst
                 "🦆","🦆",
                 "🦉","🦉",
                 "🐛","🐛",
-                "🕷️","🕷️2",
+                "🕷️","🕷️",
             };
             Random random = new Random();
 
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
                 int index = random.Next(animalEmoji.Count);
-                string nextEmoji=animalEmoji[index];
+                string nextEmoji = animalEmoji[index];
                 textBlock.Text = nextEmoji;
                 animalEmoji.RemoveAt(index);
             }
+        }
 
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
         }
     }
 }
